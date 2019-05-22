24479710eb0307e61b0d2707fffb45b1781a7f10
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-05-22 23:33:15 +0100

    added prop types for Curtain component

--- src/components/Curtain/Curtain.js
+++ src/components/Curtain/Curtain.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { animated, useSpring } from 'react-spring';
 
 import './Curtain.css';
 
-const Curtain = ({ isOpen }) => {
+const Curtain = ({ isOpen, from }) => {
   const props = useSpring({ height: isOpen ? '100%' : '0%' });
 
   return (
@@ -12,6 +13,14 @@ const Curtain = ({ isOpen }) => {
       style={props}
     />
   );
+};
+
+Curtain.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+Curtain.defaultProps = {
+  isOpen: false,
 };
 
 export default Curtain;
